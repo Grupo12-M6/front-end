e2109f6105e9f52becdc00718bf3371918aa22fa
EliseuBrito <eliseubrito@protonmail.com>
2023-02-17 22:11:52 -0300

    feat: create cardAuctionComponent

---
+++ src/components/CardAuction/index.tsx
@@ -0,0 +1,242 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Stack,
+  Text,
+  Heading,
+  Image,
+  Divider,
+  Button,
+  ButtonGroup,
+  Badge,
+  TagLabel,
+  Tag,
+  HStack,
+  TagRightIcon,
+  TagLeftIcon,
+  Flex,
+  Avatar,
+  Img,
+  Box,
+} from "@chakra-ui/react";
+import { MdAccessTime, MdArrowRightAlt } from "react-icons/md";
+
+interface ICardActionProps {
+  title: string;
+  description: string;
+  backgroundImage?: string;
+  linkImage?: string;
+  name: string;
+  miles?: string;
+  year?: string;
+  price?: string;
+}
+
+const ProductCardAuction = ({
+  title,
+  description,
+  linkImage,
+  name,
+  miles,
+  year,
+  price,
+  backgroundImage,
+}: ICardActionProps) => {
+  return (
+    <Card width="735px" height="326px" maxHeight="326px" maxWidth="735px">
+      <Image
+        src={backgroundImage}
+        width="100%"
+        overflow="hidden"
+        position="relative"
+        filter="auto"
+        brightness="40%"
+      />
+      <Box
+        position="absolute"
+        display="flex"
+        flexDirection="column"
+        width="100%"
+        height="100%"
+        justifyContent="space-between"
+      >
+        <Box marginLeft="30px" marginTop="15px">
+          <Tag
+            size="sm"
+            key="sm"
+            borderRadius="full"
+            variant="solid"
+            colorScheme="white"
+            background="white"
+            textAlign="center"
+            alignItems="center"
+          >
+            <TagLeftIcon color="purple" as={MdAccessTime} />
+            <TagLabel
+              marginTop="5px"
+              marginBottom="5px"
+              fontSize="13px"
+              color="black"
+            >
+              00:00:00
+            </TagLabel>
+          </Tag>
+        </Box>
+        <Box marginLeft="30px" marginRight="30px" marginTop="30px">
+          <Heading noOfLines={1} size="md" color="white">
+            {title}
+          </Heading>
+          <Text noOfLines={1} color="gray.200">
+            {description}
+          </Text>
+        </Box>
+        <HStack
+          spacing="2"
+          marginLeft="30px"
+          marginBottom="15px"
+          marginTop="15px"
+        >
+          <Avatar size="sm" name={name} src={linkImage} />
+          <Text color="white">{name}</Text>
+        </HStack>
+        <HStack
+          spacing="2"
+          justifyContent="space-between"
+          marginLeft="30px"
+          marginRight="30px"
+        >
+          <Box display="flex" flexDirection="row" gap="10px">
+            <Badge
+              variant="solid"
+              colorScheme="Gray 100"
+              backgroundColor="gray.100"
+              color="purple"
+            >
+              {year}
+            </Badge>
+            <Badge
+              variant="solid"
+              colorScheme="Gray 100"
+              backgroundColor="gray.100"
+              color="purple"
+            >
+              {miles}
+            </Badge>
+          </Box>
+
+          <Text color="white">R$ {price}</Text>
+        </HStack>
+        <Box>
+          <Button
+            justifyContent="space-between"
+            colorScheme="purple"
+            borderRadius="none"
+            width="100%"
+          >
+            <Text marginLeft="10px" fontSize="12">
+              Acessar página do leilão
+            </Text>
+            <TagRightIcon marginRight="10px" as={MdArrowRightAlt} />
+          </Button>
+        </Box>
+      </Box>
+    </Card>
+    // <Card
+    //   display="flex"
+    //   flexDirection="column"
+    //   // maxW={735}
+    //   // maxH={326}
+    //   width="745px"
+    //   height="326px"
+    // >
+    //   {/* <Img
+    //     filter="auto"
+    //     brightness="50%"
+    //     position="relative"
+    //     src={backgroundImage}
+    //   ></Img> */}
+
+    //   <CardBody
+    //     // backgroundImage={backgroundImage}
+    //     // backgroundPosition="center center"
+    //     // backgroundSize="cotain"
+    //     // filter="auto"
+    //     position="absolute"
+    //   >
+    //     <Tag
+    //       size="sm"
+    //       key="sm"
+    //       borderRadius="full"
+    //       variant="solid"
+    //       colorScheme="white"
+    //       background="white"
+    //       textAlign="center"
+    //       alignItems="center"
+    //     >
+    //       <TagLeftIcon color="purple" as={MdAccessTime} />
+    //       <TagLabel
+    //         marginTop="5px"
+    //         marginBottom="5px"
+    //         fontSize="13px"
+    //         color="black"
+    //       >
+    //         00:00:00
+    //       </TagLabel>
+    //     </Tag>
+    //     <Box
+    //       display="flex"
+    //       flexDirection="column"
+    //       marginTop="40px"
+    //       marginBottom="15px"
+    //       gap="15px"
+    //     >
+    //       <Heading noOfLines={1} size="md" color="white">
+    //         {title}
+    //       </Heading>
+    //       <Text noOfLines={1} color="gray.200">
+    //         {description}
+    //       </Text>
+    //     </Box>
+
+    //     <HStack spacing="2" marginBottom="15px" marginTop="15px">
+    //       <Avatar size="sm" name={name} src={linkImage} />
+    //       <Text color="white">{name}</Text>
+    //     </HStack>
+    //     <HStack
+    //       spacing="2"
+    //       justifyContent="space-between"
+    //       marginLeft="10px"
+    //       marginRight="10px"
+    //     >
+    //       <Box>
+    //         <Badge
+    //           variant="solid"
+    //           colorScheme="Gray 100"
+    //           backgroundColor="gray.100"
+    //           color="purple"
+    //         >
+    //           {tags}
+    //         </Badge>
+    //       </Box>
+    //       <Text color="white">R$ {price}</Text>
+    //     </HStack>
+    //     <HStack spacing="2"></HStack>
+    //   </CardBody>
+
+    //   <Button
+    //     justifyContent="space-between"
+    //     colorScheme="purple"
+    //     borderRadius="none"
+    //   >
+    //     <Text marginLeft="10px" fontSize="12">
+    //       Acessar página do leilão
+    //     </Text>
+    //     <TagRightIcon marginRight="10px" as={MdArrowRightAlt} />
+    //   </Button>
+    // </Card>
+  );
+};
+
+export default ProductCardAuction;
