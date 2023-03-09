e9aedb634e1e7de3aeb85f422c1dcccf499a8d81
EliseuBrito <eliseubrito@protonmail.com>
2023-03-09 12:52:47 -0300

    feat: create activation page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,13 +3,17 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import Profile from "../pages/Profile";
 import Product from "../pages/Product";
+import Activate from "../pages/Activate";
+import ResetPassword from "../pages/ResetPassword";
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/users/:id" element={<Profile />} />
+      <Route path="/users/activate/:id" element={<Activate />} />
       <Route path="/ad/:id" element={<Product />} />
+      <Route path="/resetPassword" element={<ResetPassword />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
